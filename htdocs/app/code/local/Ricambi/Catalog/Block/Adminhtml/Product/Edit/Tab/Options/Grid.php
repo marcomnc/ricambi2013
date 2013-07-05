c153cae7d194c4cb7f74c2101db31fa45752ec96
doctor <doctor@TARDIS>
2013-07-05 02:26:46 +0200

    Pubblicazione gestione opzioni

---
+++ htdocs/app/code/local/Ricambi/Catalog/Block/Adminhtml/Product/Edit/Tab/Options/Grid.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *
+ * @category    
+ * @package        
+ * @copyright   Copyright (c) 2013 Mps Sistemi (http://www.mps-sistemi.it)
+ * @author      MPS Sistemi S.a.s - Marco Mancinelli <marco.mancinelli@mps-sistemi.it>
+ *
+ */
+class Ricambi_Catalog_Block_Adminhtml_Product_Edit_Tab_Options_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+    
+    public function __construct() {
+        parent::__construct();
+        $this->setUseAjax(true);
+        $this->setId('option_product_links');
+
+        if (!is_null($this->getAssociateProduct())) {
+            $this->setDefaultFilter(array('in_products'=>1));
+        }
+    }
+    
+    protected function _addColumnFilterToCollection($column)
+    {
+        if ($column->getId() == 'in_products') {
+            $productIds = $this->_getSelectedProducts();
+
+            if (empty($productIds)) {
+                $productIds = 0;
+            }
+
+            $existsProducts = $productIds; // Only for "Yes" Filter we will add created products
+
+            if ($column->getFilter()->getValue()) {
+                $this->getCollection()->addFieldToFilter('entity_id', array('in'=>$existsProducts));
+            }
+            else {
+                if($productIds) {
+                    $this->getCollection()->addFieldToFilter('entity_id', array('nin'=>$productIds));
+                }
+            }
+        }
+        else {
+            parent::_addColumnFilterToCollection($column);
+        }
+        return $this;
+    }
+    
+     /**
+     * Prepare collection
+     *
+     * @return Mage_Adminhtml_Block_Catalog_Product_Edit_Tab_Super_Config_Grid
+     */
+    protected function _prepareCollection()
+    {
+        $allowProductTypes = array(Mage_Catalog_Model_Product_Type::DEFAULT_TYPE); //Solo semplici
+        
+        $collection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('attribute_set_id')
+            ->addAttributeToSelect('type_id')
+            ->addAttributeToSelect('price')
+            ->addFieldToFilter('type_id', $allowProductTypes)   
+            ->joinAttribute('name', 'catalog_product/name', 'entity_id', null, 'inner');
+        
+        if (!is_null($this->getAssociateProduct())) {
+            $collection->addFieldToFilter('entity_id', array('nin' => $this->getAssociateProduct()->getId()));
+        }
+
+        if (Mage::helper('catalog')->isModuleEnabled('Mage_CatalogInventory')) {
+            Mage::getModel('cataloginventory/stock_item')->addCatalogInventoryToProductCollection($collection);
+        }
+
+
+        $this->setCollection($collection);
+
+        parent::_prepareCollection();
+        return $this;
+    }
+    
+    protected function _getSelectedProducts()
+    {
+        return (!is_null($this->getActualStructure())) ? $this->getActualStructure() : array();
+    }
+    
+    
+    
+    protected function _prepareColumns()
+    {
+
+        $this->addColumn('in_products', array(
+            'header_css_class' => 'a-center',
+            'type'      => 'checkbox',
+            'name'      => 'in_products',
+            'values'    => $this->_getSelectedProducts(),
+            'align'     => 'center',
+            'index'     => 'entity_id',
+            'renderer'  => 'adminhtml/catalog_product_edit_tab_super_config_grid_renderer_checkbox',
+        ));
+        
+
+        $this->addColumn('entity_id', array(
+            'header'    => Mage::helper('catalog')->__('ID'),
+            'sortable'  => true,
+            'width'     => '60px',
+            'index'     => 'entity_id'
+        ));
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('catalog')->__('Name'),
+            'index'     => 'name'
+        ));
+
+
+        $sets = Mage::getModel('eav/entity_attribute_set')->getCollection()            
+            ->load()
+            ->toOptionHash();
+
+        $this->addColumn('set_name',
+            array(
+                'header'=> Mage::helper('catalog')->__('Attrib. Set Name'),
+                'width' => '130px',
+                'index' => 'attribute_set_id',
+                'type'  => 'options',
+                'options' => $sets,
+        ));
+
+        $this->addColumn('sku', array(
+            'header'    => Mage::helper('catalog')->__('SKU'),
+            'width'     => '80px',
+            'index'     => 'sku'
+        ));
+
+        $this->addColumn('price', array(
+            'header'    => Mage::helper('catalog')->__('Price'),
+            'type'      => 'currency',
+            'currency_code' => (string) Mage::getStoreConfig(Mage_Directory_Model_Currency::XML_PATH_CURRENCY_BASE),
+            'index'     => 'price'
+        ));
+
+        $this->addColumn('is_saleable', array(
+            'header'    => Mage::helper('catalog')->__('Inventory'),
+            'renderer'  => 'adminhtml/catalog_product_edit_tab_super_config_grid_renderer_inventory',
+            'filter'    => 'adminhtml/catalog_product_edit_tab_super_config_grid_filter_inventory',
+            'index'     => 'is_saleable'
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    
+    
+    /**
+     * Retrieve currently edited product object
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _getProduct()
+    {
+        return Mage::registry('current_product');
+    }
+    
+    
+}
+
+?>
