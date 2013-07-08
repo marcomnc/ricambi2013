50ca969cd50b6ea87981847420ca927523db760c
doctor <doctor@TARDIS>
2013-07-08 17:52:19 +0200

    Gestione allegati

--- htdocs/app/code/community/Uni/Fileuploader/Model/Fileuploader.php
+++ htdocs/app/code/community/Uni/Fileuploader/Model/Fileuploader.php
@@ -27,7 +27,22 @@ class Uni_Fileuploader_Model_Fileuploader extends Mage_Core_Model_Abstract {
         $collection = Mage::getResourceModel('fileuploader/fileuploader_collection');
         $collection->addFieldToFilter('product_ids', array('finset' => $productId))        
                 ->addFieldToFilter('file_status', 1);
-        $collection->getSelect()->order('sort_order');
+        $collection->getSelect()
+                   ->joinLeft(array('_label' => Mage::getSingleton('core/resource')->getTableName('fileuploader/label')),
+                              "_label.fileuploader_id = main_table.fileuploader_id and _label.store_id = " . MAge::app()->getStore()->getId())
+                    ->joinLeft(array('_label_default' => Mage::getSingleton('core/resource')->getTableName('fileuploader/label')),
+                              "_label_default.fileuploader_id = main_table.fileuploader_id and _label_default.store_id = 0")
+                    ->reset(Varien_Db_Select::COLUMNS)                   
+                    ->columns(array('main_table.fileuploader_id', 
+                                    'main_table.uploaded_file', 
+                                    'main_table.file_content', 
+                                    'main_table.product_ids', 
+                                    'main_table.file_status', 
+                                    'main_table.content_disp', 
+                                    'main_table.sort_order', 
+                                    'main_table.update_time',
+                                    "coalesce(coalesce(_label.value,_label_default.value), main_table.title) as title"))
+                    ->order('sort_order');
         return $collection->toArray();
     }
     
